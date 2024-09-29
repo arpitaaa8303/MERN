700920e10617d2a37ac26c863febff8183697328
arpitaaa8303 <arpitaaa8303@gmail.com>
2024-09-29 11:55:53 +0530

    modification utils data

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,8 +1,8 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message="something went wrong",
-        errors =[],
+        message= "Something went wrong",
+        errors = [],
         stack = ""
     ){
         super(message)
@@ -12,11 +12,12 @@ class ApiError extends Error {
         this.success = false;
         this.errors = errors
 
-        if(stack){
+        if (stack) {
             this.stack = stack
-        }else{
+        } else{
             Error.captureStackTrace(this, this.constructor)
         }
+
     }
 }
 
